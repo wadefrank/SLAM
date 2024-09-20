a7be0e8408a465802ab381bc623150a930d00cc5
WadeFrank <wadefrank@163.com>
2024-09-20 18:52:34 +0800

    update useEigenGeometry

--- eigen/src/useEigenGeometry.cpp
+++ eigen/src/useEigenGeometry.cpp
@@ -32,6 +32,31 @@ int main(int argc, char **argv)
     Eigen::Vector3d euler_angles = rotation_matrix.eulerAngles(2, 1, 0); // ZYX顺序，即yaw-pitch-roll顺序
     std::cout << "yaw pitch roll = " << euler_angles.transpose() << std::endl;
 
+    // 欧氏变换矩阵使用 Eigen::Isometry
+    Eigen::Isometry3d T = Eigen::Isometry3d::Identity();    // 虽然称为3d，实质上是4＊4的矩阵
+    T.rotate(rotation_vector);                              // 按照rotation_vector进行旋转
+    T.pretranslate(Eigen::Vector3d(1, 3, 4));                      // 把平移向量设成(1,3,4)
+    std::cout << "Transform matrix = \n" << T.matrix() << std::endl;
+
+    // 用变换矩阵进行坐标变换
+    Eigen::Vector3d v_transformed = T * v;                              // 相当于R*v+t
+    std::cout << "v tranformed = " << v_transformed.transpose() << std::endl;
+
+    // 对于仿射和射影变换，使用 Eigen::Affine3d 和 Eigen::Projective3d 即可，略
+
+    // 四元数
+    // 可以直接把AngleAxis赋值给四元数，反之亦然
+    Eigen::Quaterniond q = Eigen::Quaterniond(rotation_vector);
+    std::cout << "quaternion from rotation vector = " << q.coeffs().transpose()
+              << std::endl;   // 请注意coeffs的顺序是(x,y,z,w),w为实部，前三者为虚部
+    // 也可以把旋转矩阵赋给它
+    q = Eigen::Quaterniond(rotation_matrix);
+    std::cout << "quaternion from rotation matrix = " << q.coeffs().transpose() << std::endl;
+    // 使用四元数旋转一个向量，使用重载的乘法即可
+    v_rotated = q * v; // 注意数学上是qvq^{-1}
+    std::cout << "(1,0,0) after rotation = " << v_rotated.transpose() << std::endl;
+    // 用常规向量乘法表示，则应该如下计算
+    std::cout << "should be equal to " << (q * Eigen::Quaterniond(0, 1, 0, 0) * q.inverse()).coeffs().transpose() << std::endl;
 
     return 0;
 }
